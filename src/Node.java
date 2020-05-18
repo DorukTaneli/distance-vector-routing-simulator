7420dd00fa0c9d1f3827bd4666b4059b52abc0e0
Aslı Ağılönü <MissAA@users.noreply.github.com>
2020-05-18 22:09:11 +0300

    main class structure for node created, test text added

--- src/Node.java
+++ src/Node.java
@@ -1,2 +1,35 @@
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+
 public class Node {
+
+    private int nodeID;
+    Hashtable<Integer, Integer> linkCost;
+    Hashtable<Integer, Integer> linkBandwidth;
+    private List<Integer> distanceTable;
+
+    public Node(int nodeID, Hashtable<Integer, Integer> linkCost, Hashtable<Integer, Integer> linkBandwidth) {
+        this.nodeID = nodeID;
+        this.linkCost = linkCost;
+        this.linkBandwidth = linkBandwidth;
+        distanceTable = new ArrayList<Integer>();
+    }
+
+    //TODO
+    public void receiveUpdate(Message m){
+
+    }
+
+    //TODO
+    public boolean sendUpdate(){
+        return false;
+    }
+
+    //TODO
+    public Hashtable<String, String> getForwardingTable(){
+        Hashtable<String, String> forwardingTable = new Hashtable<String, String>();
+
+        return forwardingTable;
+    }
 }
