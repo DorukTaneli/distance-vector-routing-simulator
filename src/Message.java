e0b5b6f3a6ad2ae48633d91a826ae5ec1d5cb6ee
Aslı Ağılönü <MissAA@users.noreply.github.com>
2020-05-20 15:59:03 +0300

    Changed file read method

--- src/Message.java
+++ src/Message.java
@@ -2,5 +2,7 @@ public class Message {
 
     private int senderID;
     private int receiverID;
+    private int linkBandwith;
+
 
 }
--- src/ModivSim.java
+++ src/ModivSim.java
@@ -1,5 +1,4 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Scanner;
@@ -13,18 +12,37 @@ public class ModivSim {
         // Read the topology from file. Each line in the text represents a node. It assumes that all the nodes are contained in the single file
         Hashtable<Integer, Integer> linkCost;
         Hashtable<Integer, Integer> linkBandwidth;
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the file name to create the router:");
+        String user_input = null;
         try {
-            File nodeFile = new File("test.txt");
-            Scanner myReader = new Scanner(nodeFile);
-            while (myReader.hasNextLine()) {
-                String nodeInfo = myReader.nextLine();
-                System.out.println(nodeInfo);
-            }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
+            user_input = reader.readLine();
+        } catch (IOException e) {
             e.printStackTrace();
         }
+        while(!user_input.equalsIgnoreCase("done")){
+            System.out.println(user_input);
+            try {
+                File nodeFile = new File(user_input);
+                Scanner myReader = new Scanner(nodeFile);
+                String nodeInfo = myReader.nextLine();
+                System.out.println(nodeInfo);
+                myReader.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+            System.out.println("Node created. Enter another file name or type 'done':");
+            try {
+                user_input = reader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Topology created.");
+
+
     }
 
 
